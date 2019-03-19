6495cec5c45a80a6b4a8a003366c724b2d2cc8d2
Kjetil Olsen Lye <kjetil.o.lye@gmail.com>
2019-03-19 11:02:34 +0100

    fixes

---
+++ nvidia/git/make_docker.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+set -e
+LATEST_GIT_VERSION=`git ls-remote https://github.com/alsvinn/alsvinn.git HEAD | awk '{ print $1}'`
+docker build .  -t alsvinn/alsvinn_cuda_git:${LATEST_GIT_VERSION} --build-arg ALSVINN_GIT_VERSION=${LATEST_GIT_VERSION}
+docker login
+docker push alsvinn/alsvinn_cuda_git:${LATEST_GIT_VERSION}
