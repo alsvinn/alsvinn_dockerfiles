6061658cb9dce874959e126cf5cfa6dff04ab5b4
Kjetil Olsen Lye <kjetil.o.lye@gmail.com>
2019-05-07 14:46:04 +0200

    now tagging latest git version

--- nvidia/git/make_docker.sh
+++ nvidia/git/make_docker.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -e
 LATEST_GIT_VERSION=`git ls-remote https://github.com/alsvinn/alsvinn.git HEAD | awk '{ print $1}'`
-docker build .  -t alsvinn/alsvinn_cuda_git:${LATEST_GIT_VERSION} --build-arg ALSVINN_GIT_VERSION=${LATEST_GIT_VERSION}
+docker build .  -t alsvinn/alsvinn_cuda_git:latest -t alsvinn/alsvinn_cuda_git:${LATEST_GIT_VERSION} --build-arg ALSVINN_GIT_VERSION=${LATEST_GIT_VERSION}
 docker login
 docker push alsvinn/alsvinn_cuda_git:${LATEST_GIT_VERSION}
+docker push alsvinn/alsvinn_cuda_git:latest
